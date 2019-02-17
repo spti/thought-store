fabcaae815bca409619d4a84056706223fb8e12d
wsa <dannvx@gmail.com>
2019-02-17 22:34:58 +0200

    this works

--- src/convert.js
+++ src/convert.js
@@ -4,31 +4,40 @@ function formatDoc(doc) {
   // valPairs.forEach(pair => docFormatted[pair.fieldName] = pair.value)
 
   doc.val.forEach((pair) => {
-    [docFormatted[pair.fieldName]]: formatValue(pair.value)
+    docFormatted[pair.fieldName.val.value] = formatValue(pair.value)
   })
+
+  return docFormatted
 }
 
-function formatValue(value) {
-  const valueFormatted = value.val.map((rule) => {
+function formatValue(rule) {
+  console.log("formatValue, root rule", rule);
+
+  const valueFormatted = rule.val.map((rule) => {
+    console.log("formatValue, rule", rule);
     if (rule.rule === "text") {
       return rule.val.value
     } else if (rule.rule === "document_pair") {
       return {
         edgeLabel: rule.fieldName.val.value,
-        document: formatDoc(rule.value.val)
+        document: formatDoc(rule.val)
       }
     } else if (rule.rule === "document") {
       return formatDoc(rule.val)
     }
   })
 
+  return valueFormatted
+
 }
 
 const mod = {
   format: (syntaxTree) => {
-    console.log(syntaxTree)
-
     const docs = syntaxTree.map(node => formatDoc(node))
+
+    console.log("format, formatted docs:", docs);
+    return docs
+
   },
 
 }
