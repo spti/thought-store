3bee415b7cde6aab9294d3a2e4c79f53e73cae9b
wsa <dannvx@gmail.com>
2019-02-19 10:21:19 +0200

    clean up

---
+++ src/helpers.js
@@ -0,0 +1,53 @@
+function formatDoc(doc) {
+  const docFormatted = {}
+  // const valPairs = doc.val.map(pair => formatValPair(pair))
+  // valPairs.forEach(pair => docFormatted[pair.fieldName] = pair.value)
+
+  doc.val.forEach((pair) => {
+    docFormatted[pair.fieldName.val.value] = formatValue(pair.value)
+  })
+
+  return docFormatted
+}
+
+function formatValue(rule) {
+  console.log("formatValue, root rule", rule);
+
+  const valueFormatted = rule.val.map((rule) => {
+    console.log("formatValue, rule", rule);
+    if (rule.rule === "text") {
+      return {
+        type: 'text',
+        value: rule.val.value
+      }
+    } else if (rule.rule === "document_pair") {
+      return {
+        type: "document",
+        value: {
+          edgeLabel: rule.fieldName.val.value,
+          document: formatDoc(rule.val)
+        }
+      }
+    } else if (rule.rule === "document") {
+      return { // return formatDoc(rule)
+        type: "document",
+        value: {
+          edgeLabel: "defaultLabel",
+          document: formatDoc(rule)
+        }
+      }
+    }
+  })
+
+  return valueFormatted
+
+}
+
+function format(syntaxTree) {
+  const docs = syntaxTree.map(node => formatDoc(node))
+
+  console.log("format, formatted docs:", docs);
+  return docs
+}
+
+module.exports = {format}
